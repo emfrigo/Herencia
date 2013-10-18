e0c1fc29418c3a61bc748e566bc1b918a5319dcb
emfrigo <emfrigo@hotmail.com>
2013-10-17 20:06:36 -0500

    Inicio de la tarea, agregado de las clase de herencia

---
+++ Empleado/Empleado.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Empleado
+{
+	class MainClass
+	{
+		private String nombre;
+		private String puesto;
+		private double sueldo;
+
+		public virtual void pagar(){
+
+		}
+}
---
+++ Empleado/Obrero.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Empleado
+{
+	public class Obrero
+	{
+		private int horasT;
+		private double sueldoXH;
+
+		public Obrero ()
+		{
+		}
+
+		public override void pagar(){
+
+		}
+}
+
